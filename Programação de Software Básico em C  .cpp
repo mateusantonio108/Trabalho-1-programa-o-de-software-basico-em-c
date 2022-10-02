3f0496ea7d93b8a8faa76cee4f986dd08646cff3
mateusantonio108 <82397762+mateusantonio108@users.noreply.github.com>
2022-10-02 06:48:18 -0300

    Add files via upload

---
+++ Programação de Software Básico em C  .cpp
@@ -0,0 +1,20 @@
+/*
+ Author: Mateus Pontes
+ Date: 01/Octuber/2022 
+ Hour:19:00
+ Project:Program that simulates a cafeteria.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
+
+int codprod, op, op2, qtd, qtdref, qtdsobre;
+char *descricao, *descrrefre, *descrsobre;
+float valor, valortotal, valorsobre,valorrefre,totalGeral;
+ struct produto
+ {
+    char descricao, descrerefri,descrsobre;
+    float valor, valortotal,valorsobre, valorrefri,total;
+ };
+ 
