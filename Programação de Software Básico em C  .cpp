15c8d49c443797a5cbd91ced13d215e3fc56dcf9
mateusantonio108 <82397762+mateusantonio108@users.noreply.github.com>
2022-10-04 16:26:43 -0300

    Update Programação de Software Básico em C  .cpp

--- Programação de Software Básico em C  .cpp
+++ Programação de Software Básico em C  .cpp
@@ -69,4 +69,116 @@ void bebidas(){
 		
 	}
 }
+void cardapio(){
+	printf(" ==================================\n");
+	printf("|  ESCOLHA O LANCHE DESEJADO       |\n");
+	printf("|----------------------------------|\n");
+	printf("|  Codigo   | Descricao   | Valor  |\n");
+	printf("|----------------------------------|\n");
+	printf("|    1      | Hamburg     | 5.50   |\n");
+	printf("|----------------------------------|\n");
+	printf("|    2      | Misto       | 4.50   |\n");
+	printf("|----------------------------------|\n");
+	printf("|    3      | Hot-dog     | 6.50   |\n");
+	printf("|----------------------------------|\n");
+	printf("|    4      | Cupcake     |  5.00  |\n");
+	printf("|----------------------------------|\n");
+	printf("|    5      | Cookie      |  3.00  |\n");
+	printf("|==================================|\n");
+	printf("Informe o código do produto \n");
+	scanf("%d",&codprod);	
+	switch(codprod){
+		case 1:{
+			   descricao="Hamburg";
+			 valor = 5.50;
+			 printf("informe a quantidade \n");
+			 scanf("%d",&qtd); 
+			break;
+		}
+		case 2:{
+			   descricao="Misto";
+			 valor = 4.50;
+			 printf("informe a quantidade \n");
+			 scanf("%d",&qtd);
+			break;
+		}
+		case 3:{
+			   descricao="Hot-Dog";
+			 valor = 6.50;
+			 printf("informe a quantidade \n");
+			 scanf("%d",&qtd);
+			break;
+		}
+		case 4:{
+			  descrrefre="Cupcake";
+			 valorrefre = 5.00;
+			 printf("informe a quantidade \n");
+			 scanf("%d",&qtdref);
+			break;
+		}
+		case 5:{
+			 descrrefre="Cookie";
+			 valorrefre = 3.00;
+			 printf("informe a quantidade \n");
+			 scanf("%d",&qtdref);
+			break;
+		}		
+		
+	}
+	printf("Deseja pedir algum refrigerante ou outro item ? 1-Sim ou Zero para nÃ£o\n");
+	scanf("%d",&op);
+	system("cls");
+	if (op==1){
+		bebidas();
+	}else{
+	   cardapio();
+	    	
+	}
+      
+}
+void visualizarCupom(){
+	printf("|=================================|\n");
+	printf("|      CONFIRA SEU CUPOM          |\n");
+	printf("==================================\n");
+	printf("|Descricao sanduiche  : %s\n",descricao);
+	printf("|Quant sanduiche      : %d\n",qtd);
+	printf("|Valor Unit sanduiche : %f\n",valor);
+	printf("|Descricao refr.      : %s\n",descrrefre);
+	printf("|Quant refri          : %d\n",qtdref);
+	printf("|Valor Unit refri     : %f\n",valorrefre);
+	printf("|=================================|\n");
+	printf("|             TOTAIS              |\n");
+	printf("==================================\n");
+	printf("|Total item sanduiche : R$ %f \n", qtd*valor);
+	printf("|Total item refri     : R$ %f \n", qtdref*valorrefre);
+	printf("|Total a pagar        : R$ %f \n", ((qtd*valor)+(qtdref*valorrefre)));
+	printf("|=================================|\n");
+	printf("|   OBRIGADO PELA PREFERÃŠNCIA     |\n");
+	printf("==================================\n");
+}
+
+main(){
+	MenuPrincipal();
+	switch(op2){
+		case 1:{
+			cardapio();
+			visualizarCupom();
+			break;
+		}
+		case 2:{
+			visualizarCupom();
+			
+			break;
+		}
+		case 3:{
+			//AlterarPedido();
+			system("cls");
+			break;
+		}
+		case  4:{
+			//Consultar();
+			break;
+		}
+	}	
+}
 
